973a8648869380ce12a2b5c6991adb904fd8b36b
PrajulVP <149560534+PrajulVP@users.noreply.github.com>
2025-11-11 17:37:33 +0530

    Feature: Add new permissions for Retailer Orders and Distributor Bulk Orders

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Permission; // Use our extended Permission model
 use App\Models\PermissionCategory;
+use App\Models\PermissionGroup; // Import PermissionGroup model
 
 class PermissionSeeder extends Seeder
 {
@@ -15,6 +16,9 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
+        // Find or create the "Orders" permission group
+        $ordersGroup = PermissionGroup::firstOrCreate(['name' => 'Orders']);
+
         $permissionCategories = PermissionCategory::all();
         $actions = ['view', 'add', 'edit', 'delete'];
 
@@ -31,6 +35,69 @@ class PermissionSeeder extends Seeder
                         ]
                     );
                 }
+            }
+        }
+
+        // Add new categories and their permissions
+        $newCategories = [
+            [
+                'name' => 'Retailer Orders',
+                'short_code' => 'retailer_orders',
+                'enable_view' => true,
+                'enable_add' => true,
+                'enable_edit' => true,
+                'enable_delete' => true,
+                'perm_group_id' => $ordersGroup->id, // Assign to Orders group
+            ],
+            [
+                'name' => 'Distributor Bulk Orders',
+                'short_code' => 'distributor_bulk_orders',
+                'enable_view' => true,
+                'enable_add' => true,
+                'enable_edit' => true,
+                'enable_delete' => true,
+                'perm_group_id' => $ordersGroup->id, // Assign to Orders group
+            ],
+        ];
+
+        foreach ($newCategories as $newCategoryData) {
+            $category = PermissionCategory::firstOrCreate(
+                ['short_code' => $newCategoryData['short_code']],
+                $newCategoryData
+            );
+
+            foreach ($actions as $action) {
+                $enableFlag = 'enable_' . $action;
+                if ($category->$enableFlag) {
+                    $permissionName = $action . ' ' . $category->short_code;
+                    Permission::firstOrCreate(
+                        [
+                            'name' => $permissionName,
+                            'guard_name' => 'web',
+                            'permission_category_id' => $category->id,
+                        ]
+                    );
+                }
+            }
+        }
+
+        // Add specific permissions for Retailer Orders
+        $retailerOrdersCategory = PermissionCategory::where('short_code', 'retailer_orders')->first();
+        if ($retailerOrdersCategory) {
+            $specificRetailerOrderPermissions = [
+                'assign_distributor retailer_orders',
+                'assign_fieldstaff retailer_orders',
+                'update_delivery_status retailer_orders',
+            ];
+
+            foreach ($specificRetailerOrderPermissions as $permissionName) {
+                Permission::firstOrCreate(
+                    [
+                        'name' => $permissionName,
+                        'guard_name' => 'web',
+                        'permission_category_id' => $retailerOrdersCategory->id,
+                    ]
+                );
             }
         }
     }
