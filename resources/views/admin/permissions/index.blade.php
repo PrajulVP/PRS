6c50a31c8c237373bf25c02150df0722991b33e5
PrajulVP <149560534+PrajulVP@users.noreply.github.com>
2025-11-11 19:09:32 +0530

    Feature: Restrict 'Manage Permissions' link visibility based on 'edit permissions' permission

--- resources/views/admin/permissions/index.blade.php
+++ resources/views/admin/permissions/index.blade.php
@@ -30,9 +30,11 @@
                         @foreach($roles as $role)
                             <div class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
                                 {{ ucfirst($role->name) }}
+                                @can('edit permissions')
                                 <a href="{{ route('admin.permissions.edit', $role) }}" class="btn btn-primary btn-sm">
                                     Manage Permissions
                                 </a>
+                                @endcan
                             </div>
                         @endforeach
                     </div>
