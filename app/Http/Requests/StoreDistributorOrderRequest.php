ed4c124de41682a696fff0e8ca58868c2391ddef
Sreyas-A-S <sreyasas25@gmail.com>
2025-12-30 14:15:06 +0530

    Changes

--- app/Http/Requests/StoreDistributorOrderRequest.php
+++ app/Http/Requests/StoreDistributorOrderRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 
-class StoredistributorOrderRequest extends FormRequest
+class StoreDistributorOrderRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
