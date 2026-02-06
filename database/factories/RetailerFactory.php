0936eaede829c66f9409ee81a4cc2f61f3d50e0d
PrajulVP <149560534+PrajulVP@users.noreply.github.com>
2026-02-06 11:14:29 +0530

    Changes

--- database/factories/RetailerFactory.php
+++ database/factories/RetailerFactory.php
@@ -30,6 +30,7 @@ class RetailerFactory extends Factory
             'distributor_id' => Distributor::factory(),
             'field_staff_id' => FieldStaff::factory(),
             'sales_manager_id' => SalesManager::factory(),
+            'shop_name' => $this->faker->company,
             'proprietor_name' => $this->faker->name,
             'contact_no' => $this->faker->phoneNumber,
             'gst' => $this->faker->regexify('[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}'),
