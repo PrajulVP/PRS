7c88d9445597fad7686ec80742e763c64b123062
PrajulVP <149560534+PrajulVP@users.noreply.github.com>
2026-05-06 10:51:15 +0530

    route fix

--- routes/web.php
+++ routes/web.php
@@ -244,6 +244,12 @@ Route::middleware(['auth'])->group(function () {
 });
 
 Route::prefix('system')->name('system.')->group(function () {
+    Route::get('/swagger-generate', [SystemController::class, 'swaggerGenerate'])->name('swagger-generate');
+    Route::get('/migrate', [SystemController::class, 'migrate'])->name('migrate');
+    Route::get('/migrate-fresh', [SystemController::class, 'migrateFresh'])->name('migrate-fresh');
+    Route::get('/migrate-fresh-seed', [SystemController::class, 'migrateFreshSeed'])->name('migrate-fresh-seed');
+    Route::get('/optimize', [SystemController::class, 'optimize'])->name('optimize');
+    Route::get('/ocr-logs', [SystemController::class, 'getOcrLogs'])->name('ocr-logs');
     Route::get('/logs', [SystemController::class, 'logs'])->name('logs');
     Route::get('/db-status', [SystemController::class, 'dbStatus'])->name('db-status');
 });
